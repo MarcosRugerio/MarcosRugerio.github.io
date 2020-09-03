abc42f8c0ddd3dc7563b2a0d02315e9c19e0e3c3
Marcos Rugerio <marcosrugerio@gmail.com>
2020-09-03 00:23:45 -0500

    Probando "Tailwind".

---
+++ ProyectoTailwind/tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+    purge: [],
+    theme: {
+        fontFamily: {
+
+            alitasTXT: ['Raleway'],
+        },
+        extend: {
+            colors: {
+
+                black: {
+                    100: "#e7cfcf",
+                    200: "#cf9e9e",
+                    300: "#b76e6e",
+                    400: "#9f3d3d",
+                    500: "#870d0d",
+                    600: "#6c0a0a",
+                    700: "#510808",
+                    800: "#360505",
+                    900: "#1b0303"
+                },
+                alitas: '#870d0d',
+                info: {
+                    50: '#a3d2ca',
+                    150: '#5eaaa8'
+                }
+            }
+        },
+    },
+    variants: {},
+    plugins: [],
+}
